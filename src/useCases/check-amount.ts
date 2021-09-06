d90c104eda9581fc32afae1333bce98610d646c8
Kalléo Pinheiro <windson.rocha@zup.com.br>
2021-09-06 11:38:25 -0300

    bugfix: transfer amount check on useCase

--- src/useCases/check-amount.ts
+++ src/useCases/check-amount.ts
@@ -1,12 +1,18 @@
 import { Operation } from '@domain/operation'
+import { OperationKind } from '@domain/operation-kind'
 
 export class CheckAmountUseCase {
   constructor(private readonly operation: Operation) {}
 
   async handle(): Promise<number> {
-    if (!this.operation.amount || this.operation.amount <= 0) {
+    const amount =
+      this.operation.type === OperationKind.TRANSFER
+        ? this.operation?.transaction?.amount
+        : this.operation.amount
+
+    if (!amount || amount <= 0) {
       throw new Error('Amount was not provided or amount value is invalid!')
     }
-    return this.operation.amount
+    return amount
   }
 }
